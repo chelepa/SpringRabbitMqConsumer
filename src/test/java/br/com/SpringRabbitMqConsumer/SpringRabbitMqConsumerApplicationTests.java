70c45f55ca2856f39b70738d66cc65bc24793b50
Claudimir Chelepa <claudimir.chelepa@compasso.com.br>
2020-03-30 17:26:51 -0300

    first commit

---
+++ src/test/java/br/com/SpringRabbitMqConsumer/SpringRabbitMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SpringRabbitMqConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
